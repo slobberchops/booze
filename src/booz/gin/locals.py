eee4683df39939e67231e8e4bf89314d6467f933
Rafe Kaplan <slobberchops@gmail.com>
2015-12-02 16:15:16 -0800

    Introduced locals module. Locals module contains GetAttr action. Locals has 'l' singleton for easy creation of GetAttr actions.

---
+++ src/booz/gin/locals.py
@@ -0,0 +1,38 @@
+# Copyright 2015 Rafe Kaplan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .. import util
+from .. import whiskey
+
+
+class GetAttr(whiskey.Action):
+
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+    def invoke(self, *args, locals=None, **kwargs):
+        if locals is None:
+            raise TypeError('Must provide \'locals\' parameter')
+        return getattr(locals, self.__name)
+
+
+@util.singleton
+class l:
+
+    def __getattr__(self, name):
+        return GetAttr(name)
---
+++ src/booz/gin/locals_test.py
@@ -0,0 +1,52 @@
+# Copyright 2015 Rafe Kaplan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from booz.gin import locals
+
+
+class SetAttrTestCase(unittest.TestCase):
+
+    action = locals.GetAttr('my_name')
+
+    def test_name(self):
+        self.assertEqual('my_name', self.action.name)
+
+    def test_invoke_with_no_locals(self):
+        with self.assertRaises(TypeError):
+            self.action.invoke(1, 2, 3, a='a', b='b', c='c')
+
+    def test_invoke_not_found(self):
+        with self.assertRaises(AttributeError):
+            self.action.invoke(1, 2, 3, a='a', b='b', c='c', locals=object())
+
+    def test_invoke(self):
+        class SimpleLocals:
+            pass
+        locals = SimpleLocals()
+        locals.my_name = 10
+        self.assertEqual(10, self.action.invoke(1, 2, 3, a='a', b='b', c='c', locals=locals))
+
+
+class LTestCase(unittest.TestCase):
+
+    def test_get_attr(self):
+        action = locals.l.my_name
+        self.assertIsInstance(action, locals.GetAttr)
+        self.assertEqual('my_name', action.name)
+
+
+if __name__ == '__main__':
+    unittest.main()
